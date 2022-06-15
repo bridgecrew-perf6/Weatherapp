05fc7bb2954b22012a7b3faf0fca011622b047bd
vivek <vivekkumarvky13@gmail.com>
2022-06-15 22:28:20 +0530

    secondbox

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,38 @@
 import "./Home.css"
-
+import axios from "axios"
 import Image from "../Images/location-pin.png"
+import Cloudy from "../Images/cloudy.png"
+import Sunny from "../Images/sunny.png"
 import { useEffect } from "react"
+import { useState } from "react"
+import Chart from "react-apexcharts";
 export const Home = ()=>{
 
-
+  let [currenttemp, setCurrent] = useState("");
+let [city, SetCity] = useState("")
+let [hourlyFor, SetHourly] = useState([])
+let [data, SetData] = useState([])
 useEffect(()=>{
     getData()
 },[])
+
+const chartData = {
+  options: {
+    chart: {
+      id: "basic-bar",
+    },
+    xaxis: {
+      categories: hourlyFor,
+    },
+  },
+
+  series: [
+    {
+      name: "series-1",
+      data: [0, 10, 20, 30, 40, 50],
+    },
+  ],
+};
 
 const getData =async ()=>{
     
@@ -17,19 +42,113 @@ const getData =async ()=>{
              "&lon=" +
              "77.2167" +
              "&exclude=minutely,alerts&units=metric&appid=e4c70ce6a6821649a416cb9521d5f4f8";
-      await fetch(link)
-        .then((resp) => resp.json())
-        .then((dataa) => {
-          console.log(dataa, "dfdkfdk")
+      axios.get(link).then((response) => {
+        let DailyForecast = response.data.daily;
+        let HourlyForecast = response.data.hourly;
+
+        //Data Formatting- Changing the temp and date as required by UI
+
+        DailyForecast.forEach((el, i) => {
+          let date = new Date(el.dt * 1000).toDateString();
+
+          DailyForecast[i].day = date.substring(0, 3);
+          DailyForecast[i].temp.max = el.temp.max.toPrecision(2);
+          DailyForecast[i].temp.min = el.temp.min.toPrecision(2);
+
+          let rise = new Date(
+            DailyForecast[i].sunrise * 1000
+          ).toLocaleTimeString();
+          let set = new Date(
+            DailyForecast[i].sunset * 1000
+          ).toLocaleTimeString();
+
+          DailyForecast[i].sunrise = rise.substring(0, 4);
+          DailyForecast[i].sunset = set.substring(0, 4);
         });
+
+        SetData( DailyForecast);
+        
+              setCurrent(DailyForecast[0].temp.day);
+        console.log(HourlyForecast, "DFdfdfd")
+
+        
+
+        let k = []
+
+        for (let i = 0; i < HourlyForecast.length; i++){
+          k.push(HourlyForecast[i].temp)
+        }
+        console.log(k, " k")
+SetHourly(k)
+
+      });
   
 }
+console.log(hourlyFor, "dfdfd")
+console.log(data, " data")
+console.log(currenttemp, " cureent temp")
+const handleClick = ()=>{
+    
+}
+  return (
+    <div id="mainhome">
+      <div id="box">
+        <img src={Image} alt="" id="locsign" />
+        <input
+          onChange={(e) => {
+            SetCity(e.target.value);
+            console.log(city);
+          }}
+          id="searchbox"
+          type="text"
+          placeholder="City Name"
+        />
+        <button onClick={handleClick}>Search</button>
+      </div>
+      <div id="secondbox">
+        {data.map((item) => (
+          <div className="databox">
+            <b>{item.day}</b>
+            <br />
+            <b>
+              {item.temp.max}° {item.temp.min}°{" "}
+            </b>
+            <br />
+            <img
+              className="imgsetting"
+              src={item.clouds >= 50 ? Cloudy : Sunny}
+              alt=""
+            />
+            <br />
+            {item.clouds >= 50 ? <b>Cloudy </b> : <b>Sunny</b>}
+          </div>
+        ))}
+      </div>
+      <div id="thirdbox">
+        <div className="graphbox">
+          <b>{currenttemp}° C</b>
+          <img
+            className="imgsetting"
+            src={currenttemp >= 50 ? Cloudy : Sunny}
+            alt=""
+          />
+        </div>
 
-  return <div id="mainhome">
-    <div id="box">
-        <img src={Image} alt="" id="locsign"/>
-        <input id="searchbox" type="text" placeholder="City Name" />
-        <button>Search</button>
+        <Chart
+          options={chartData.options}
+          series={chartData.series}
+          type="line"
+          width="700"
+        />
+      </div>
+      <div className="fourthbox">
+        <div className="pressure">
+
+        </div>
+         <div className="pressure">
+
+         </div>
+      </div>
     </div>
-  </div>
+  );
 }
